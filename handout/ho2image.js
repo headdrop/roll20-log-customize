2ca31aa2f5a9a112fc5a680bdf2980f2804d72f3
headdrop <83771426+headdrop@users.noreply.github.com>
2022-02-08 01:33:37 +0900

    ho

---
+++ handout/ho2image.js
@@ -0,0 +1,106 @@
+window.onload = function () { 
+  for (let i=0; i++; i<2) {
+    document.getElementsByClassName("desc")[i].addEventListener("blur",function(){
+    this.innerHTML = this.innerText.replace(/(【.+?】)(?!<)/gi,'<span class="bold">$1</span>');
+    this.querySelectorAll("span").forEach(element => {
+    var intxt = element.innerText;
+    if(intxt.includes("【") && intxt.includes("】")===false) {
+    element.after(intxt);
+    element.remove();
+    }
+    });
+    });
+  }
+  document.getElementById("addRow").addEventListener("click",function(){
+  var cloneNode = document.querySelector("#insane+.tabCon tbody.cont tr").cloneNode(true);
+  document.querySelector("#insane+.tabCon tbody.cont").append(cloneNode);
+  var newNode = document.querySelector("#insane+.tabCon tbody.cont tr:last-of-type");
+  newNode.querySelectorAll("td").forEach(function(value,index){
+  if (index>0) value.textContent="";
+  });
+  var rowbtn = document.createElement("span");
+  rowbtn.innerText = "X";
+  newNode.querySelector("td").append(rowbtn);
+  newNode.querySelector("td span").addEventListener("click",function(){
+  this.parentNode.parentNode.remove();
+  step();
+  },{ once: true });
+  step();
+  });
+  //function
+  function step () {
+  var stepRows = document.querySelectorAll("#insane+.tabCon tbody.cont tr");
+  stepRows.forEach(function(value,index,arr) {
+  if (index+1 > 2) {
+  value.querySelector("td").childNodes[0].textContent=index+1;// = index+1;
+  }
+  });
+  }
+  document.getElementById("down").onclick = function(){
+  var target = document.querySelector('input[name="tabmenu"]:checked+.tabCon .content');
+  var fileName = document.querySelector('input[name="tabmenu"]:checked').value+"_"+document.querySelector('input[name="tabmenu"]:checked+.tabCon .inputbox').innerHTML;
+  domtoimage.toBlob(target)
+  .then(function (blob) {
+  window.saveAs(blob, fileName+'.png');
+  });
+  }
+  //에디터로 복사
+  document.querySelector("#copy").addEventListener('click',()=>{
+  document.querySelectorAll("td>span").forEach(function(val){
+  val.display="none";
+  })
+  var target = document.querySelector('input[name="tabmenu"]:checked+.tabCon .content');
+  selectRange(target);
+  document.execCommand("copy");
+  alert("복사가 완료되었습니다. roll20 핸드아웃 편집 창에 붙여넣기하세요.\\n복사한 표는 이 페이지에 적용된 글꼴이 적용되지 않습니다.")
+  });
+  // 복사 (2)
+  document.querySelector(".copy").addEventListener('click',(e)=>{
+    var page = e.target.classList[2].replace("c-",'');
+    var tg = e.target.parentNode.parentNode.getElementsByClassName(page);
+    selectRange(tg[0]);
+    document.execCommand("copy");
+    alert("복사가 완료되었습니다. roll20 핸드아웃 편집 창에 붙여넣기하세요.\\n복사한 표는 이 페이지에 적용된 글꼴이 적용되지 않습니다.")
+  });
+  document.querySelectorAll("input, textarea").forEach((val,ind)=>{
+    val.addEventListener('change',(e)=>{
+    })
+  })
+  
+  function selectRange(obj) {
+  if (window.getSelection) {
+  var selected = window.getSelection();
+  selected.selectAllChildren(obj);
+  } else if (document.body.createTextRange) {
+  var range = document.body.createTextRange();
+  range.moveToElementText(obj);
+  range.select();
+  }
+  };
+
+  document.getElementById("addHo").addEventListener("click",function(){
+    var cloneNode = document.querySelector("#insaneHO+.tabCon .item").cloneNode(true);
+    document.querySelector("#insaneHO+.tabCon .content").append(cloneNode);
+    var newNode = document.querySelector("#insaneHO+.tabCon .item:last-child");
+    newNode.querySelector(".remove").addEventListener("click",function(){
+      this.parentNode.parentNode.remove();
+    },{ once: true });
+    newNode.querySelectorAll("input, textarea").forEach(function(val,index){
+      val.addEventListener("change",function(){
+        if (index===0) {
+          newNode.querySelector(".ho-output.front .ho-box>span:nth-child(2)").textContent=val.value;
+        } else if (index===1) {
+          newNode.querySelector(".ho-output.front .ho-box>.ho-box-content").textContent=val.value;
+        } else if (index===2) {
+          newNode.querySelector(".ho-output.back .ho-box>span:nth-child(2)").textContent=val.value;
+        } else if (index===3) {
+          newNode.querySelector(".ho-output.back .ho-box>.ho-box-content").textContent=val.value;
+        }
+      });
+    });
+  });
+
+}
+function changeHo (e) {
+  console.log(e);
+}
